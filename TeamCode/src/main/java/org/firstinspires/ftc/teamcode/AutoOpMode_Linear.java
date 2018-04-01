b130cf2c6bf66f82f0126ff12b7d88d3014f6080
qwertpas <christopher.y.x@gmail.com>
2018-04-01 11:32:16 -0700

    registered AutoOpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpMode_Linear.java
@@ -1,12 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
  * Created by chris on 4/1/18.
  */
+
+@Autonomous(name="AutoOpMode", group="Chris")
 public class AutoOpMode_Linear extends LinearOpMode{
 
     DcMotor aDrive;
